0cdc39c2298c8a570fd492d9bbf5f1d8e56382e1
LongYinan <lynweklm@gmail.com>
2021-10-30 17:15:59 +0800

    chore: replace allocator in x64 system

--- packages/node/src/lib.rs
+++ packages/node/src/lib.rs
@@ -9,19 +9,14 @@ use napi::{self, CallContext, Env, JsBuffer, JsObject, JsString, Result, Task};
 use rolldown::swc_common::{BytePos, LineCol};
 
 #[cfg(all(
-  target_arch = "x86_64",
+  not(target_arch = "x86_64"),
   not(target_env = "musl"),
-  not(target_os = "macos"),
   not(debug_assertions)
 ))]
 #[global_allocator]
 static ALLOC: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
 
-#[cfg(all(
-  target_os = "macos",
-  not(target_arch = "aarch64"),
-  not(debug_assertions)
-))]
+#[cfg(all(target_arch = "x86_64", not(debug_assertions)))]
 #[global_allocator]
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
