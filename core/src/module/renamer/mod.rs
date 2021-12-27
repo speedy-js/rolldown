da5a90f15407dd11ff2f8d7cd1232d47a994c1b4
Yunfei He <i.heyunfei@gmail.com>
2021-12-27 23:55:13 +0800

    wip: keep shorthand prop name

--- core/src/module/renamer/mod.rs
+++ core/src/module/renamer/mod.rs
@@ -2,8 +2,8 @@ use std::collections::HashMap;
 
 use swc_atoms::JsWord;
 use swc_common::SyntaxContext;
-use swc_ecma_ast::Ident;
-use swc_ecma_visit::VisitMut;
+use swc_ecma_ast::{Expr, Ident, KeyValueProp, ObjectLit, Prop, PropName, PropOrSpread};
+use swc_ecma_visit::{VisitMut, VisitMutWith};
 
 pub struct Renamer<'me> {
   pub ctxt_mapping: &'me HashMap<JsWord, SyntaxContext>,
@@ -19,5 +19,24 @@ impl<'me> VisitMut for Renamer<'me> {
         }
       }
     }
+  }
+
+  fn visit_mut_object_lit(&mut self, node: &mut ObjectLit) {
+    node.props.iter_mut().for_each(|prop_or_spread| {
+      match prop_or_spread {
+        PropOrSpread::Prop(prop) => {
+          if prop.is_shorthand() {
+            if let Prop::Shorthand(ident) = prop.as_mut() {
+              let mut key = ident.clone();
+              key.span.ctxt = SyntaxContext::empty();
+              let replacement = Box::new(Prop::KeyValue(KeyValueProp {  key: PropName::Ident(key), value: Box::new(Expr::Ident(ident.clone())) }));
+              *prop = replacement;
+            }
+          }
+        }
+        _ => {}
+      }
+    });
+    node.visit_mut_children_with(self);
   }
 }
