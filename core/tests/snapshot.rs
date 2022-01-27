3e8e4ec55d0d8d54bca9b647831b50c557cd4a59
Yunfei He <i.heyunfei@gmail.com>
2022-01-27 13:46:58 +0800

    Add basic snapshot tests

---
+++ core/tests/snapshot.rs
@@ -0,0 +1,18 @@
+use rolldown::{graph::GraphContainer, bundle::Bundle};
+
+#[test]
+fn basic() {
+  // let mut graph = GraphContainer::from_single_entry("./tests/fixtures/basic/main.js".to_owned());
+  // let mut graph = GraphContainer::from_single_entry("./tests/fixtures/symbols.js".to_owned());
+  // let mut graph = GraphContainer::from_single_entry("../testcase/custom/samples/default-export/main.js".to_owned());
+  // let mut graph = GraphContainer::from_single_entry("./tests/fixtures/conflicted/index.js".to_owned());
+  let mut graph =
+    GraphContainer::from_single_entry("./tests/fixtures/conflicted/index.js".to_owned());
+  // let mut graph =
+  //   GraphContainer::from_single_entry("../node_modules/lodash-es/lodash.js".to_owned());
+  graph.build();
+  let mut bundle = Bundle::new(graph);
+
+  let output = bundle.generate();
+  insta::assert_snapshot!(output);
+}
